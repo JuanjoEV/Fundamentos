79d340a5c05150bab813ff36ada313b21bcbb3b6
JuanjoEV <68124783+JuanjoEV@users.noreply.github.com>
2020-07-31 15:50:49 -0500

    Add files via upload

---
+++ blackjack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BlackJack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            int carta1 = aleatorio.Next(1, 10);
+            int carta2 = aleatorio.Next(1, 10);
+            int otraCarta;
+            string continuar = "s";
+            Console.WriteLine("Primera carta: " + carta1);
+            Console.WriteLine("Segunda carta: " + carta2);
+            int total = carta1 + carta2;
+            Console.WriteLine("Total: " + total);
+
+            Console.WriteLine("Otra carta? (s/n)");
+            continuar = Console.ReadLine();
+
+            while (continuar == "s" && total < 21)
+            {
+                otraCarta = aleatorio.Next(1, 10);
+                Console.WriteLine("Carta nueva:" + otraCarta);
+                total += otraCarta;
+                if (total <= 21)
+                {
+                    Console.WriteLine("Nuevo puntaje:" + total);
+                    Console.WriteLine("Otra carta? (s/n)");
+                    continuar = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Eliminado");
+                }
+            }
+
+            Console.WriteLine("El puntaje final es:" + total);
+        }
+    }
+}
